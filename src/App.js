7b00a3e17044115bd91386093f1f74d7a113d7aa
Sriramajeyam <yesoreyeram@gmail.com>
2018-09-27 00:00:17 +0100

    Bootstrap included

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        React Count Game
+        <div className="Container text-center mt-4">
+          <h1 className="display-4">React Count Game</h1>
+        </div>
       </div>
     );
   }
